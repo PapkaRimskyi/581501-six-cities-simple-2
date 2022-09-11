626d915e0b8fab8c9e1338d177e79b19416e45e9
PapkaRimskyi <magnezia98@mail.ru>
2022-09-11 22:42:39 +0300

    feat: module2-task1 first implement of --generate command

--- src/cli/commands/generate/generate.ts
+++ src/cli/commands/generate/generate.ts
@@ -1,4 +1,8 @@
+import fs from 'fs/promises';
+import dayjs from 'dayjs';
+
 import GetData from '../../helpers/get-data.js';
+import { getRandomData } from './helpers/get-random-data.js';
 
 import { CliCommand } from '../../interfaces/cli-command.js';
 import { TMockData } from '../../../@types/api-mock-data.js';
@@ -7,12 +11,82 @@ class Generate implements CliCommand {
   public name = '--generate';
 
   async run(n: string, filepath: string, url: string) {
-    console.log(n, filepath);
     const getData = new GetData(url);
     const data = await getData.get<TMockData>();
     if (data) {
-      console.log(data);
+      const createdData = this.configureMockData(data, n);
+      this.createFile(createdData, filepath);
     }
+  }
+
+  configureMockData(data: TMockData, n: string) {
+    try {
+      const counter = parseInt(n, 10);
+      const generatedData= [];
+      for (let i = 0; i < counter; i++) {
+        const name = getRandomData(data.name);
+        const description = getRandomData(data.description);
+        const createdAt = dayjs().toISOString();
+        const city = getRandomData(data.city);
+        const imgPreview = getRandomData(data.imgPreview);
+        const placePhotos = getRandomData(data.placePhotos);
+        const isPremium = getRandomData(data.isPremium);
+        const rating = getRandomData(data.rating);
+        const apartmentType = getRandomData(data.apartmentType);
+        const roomAmount = getRandomData(data.roomAmount);
+        const guestsAmount = getRandomData(data.guestsAmount);
+        const rentPrice = getRandomData(data.rentPrice);
+        const apartmentGoods = getRandomData(data.apartmentGoods);
+        const author = getRandomData(data.author);
+        const coords = getRandomData(data.coords);
+        generatedData.push(
+          [
+            name,
+            description,
+            createdAt,
+            city,
+            imgPreview,
+            placePhotos.join(';'),
+            isPremium,
+            rating,
+            apartmentType,
+            roomAmount,
+            guestsAmount,
+            rentPrice,
+            apartmentGoods.join(';'),
+            author,
+            coords,
+          ]
+        );
+        // return [
+        //   name,
+        //   description,
+        //   createdAt,
+        //   city,
+        //   imgPreview,
+        //   placePhotos.join(';'),
+        //   isPremium,
+        //   rating,
+        //   apartmentType,
+        //   roomAmount,
+        //   guestsAmount,
+        //   rentPrice,
+        //   apartmentGoods.join(';'),
+        //   author,
+        //   coords,
+        // ];
+      }
+      return generatedData;
+    } catch (e) {
+      console.error(e);
+      return [];
+    }
+  }
+
+  createFile<T>(data: Array<Array<T>>, filepath: string) {
+    data.forEach(async(item) => {
+      await fs.appendFile(filepath, `${(item).join('\t')}${'\n'}`);
+    });
   }
 }
 
